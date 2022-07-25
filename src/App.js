b11da8ff628df763336d38ee39a6160f964d4512
JuniorTIM <musa.aliev06@yandex.ru>
2022-07-25 14:48:18 +0300

    подключил роуты

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./components/pages/HomePage";
+import About from "./components/pages/About/About";
+import Contacts from "./components/pages/Contacts/Contacts";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-   return (
-      <>
-         <BrowserRouter>
-            <Routes>
-               <Route path="/" element={<HomePage />} />
-            </Routes>
-         </BrowserRouter>
-      </>
-   )
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contacts" element={<Contacts />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
