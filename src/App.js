f06fb6c71fb55adf6dd22cf58677f5df32d400ca
Zelim <anzorov.zelim@gmail.com>
2022-07-25 16:55:44 +0300

    добавил слайсы и редюсеры для услуг

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-
+import React from 'react';
+import Services from './components/pages/Services/Services'
 
 function App() {
   return (
-    <div className="App">
-      Hello World
+    <div>
+      <Services />
     </div>
   );
 }
 
 export default App;
+
+// function App() {
+//   return (
+//     <div className="App">
+//       Hello World
+//     </div>
+//   );
+// }
+
+// export default App;
--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import serviceReducer from "../features/services/servicesSlice";
 
 export const store = configureStore({
-    reducer: {
-        // usersReducer: usersReducer, 
-
-    }
-})
+  reducer: {
+    // usersReducer: usersReducer,
+    services: serviceReducer,
+  },
+});
