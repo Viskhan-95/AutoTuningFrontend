cdc87eef537f50cf8758c44c61e84db1f950b4ba
Paranoid <magomaevd99@gmail.com>
2022-07-25 14:15:01 +0300

    тестовый вариант футера

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./components/pages/HomePage";
 import Navibar from "./components/pages/Navibar/Navibar";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Footer from "./components/Footer/Footer";
 
 function App() {
    return (
@@ -11,6 +12,7 @@ function App() {
             <Routes>
                <Route path="/" element={<HomePage />} />
             </Routes>
+            < Footer />
          </BrowserRouter>
       </>
    )
