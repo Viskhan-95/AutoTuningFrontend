1ae1580d1ce88c646bbd5fd9065ee392f67ef5f6
viskhan <viskhan.rkb@gmail.com>
2022-07-25 14:20:11 +0300

    сделал навбар и фон главной страницы

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./components/pages/HomePage";
-import Navibar from "./components/pages/Navibar/Navibar";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
    return (
       <>
          <BrowserRouter>
-            <Navibar />
             <Routes>
                <Route path="/" element={<HomePage />} />
             </Routes>
--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,11 +1,20 @@
 import react from 'react';
 import { Container } from 'react-bootstrap';
-
+import backgroundImage from "../../icon/backgroundImg.jpeg";
+import Navibar from "./Navibar/Navibar";
 
 const HomePage = () => {
    return (
       <>
-         
+         <Container fluid style={{
+            backgroundImage: `url(${backgroundImage}`,
+            height: "100vh",
+            width: "100%",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover"
+         }}>
+         <Navibar />
+         </Container>
       </>
    )
 };
