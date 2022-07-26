f708c24cb5b4da6fc9d11e0d8d9e96fac8cd5f91
Djabrail Magamadov <magamadov.ru095@gmail.com>
2022-07-26 10:59:39 +0300

    Добавил услуги и исправил ошибки

--- src/components/pages/About/About.jsx
+++ src/components/pages/About/About.jsx
@@ -2,11 +2,22 @@ import React from 'react';
 import Footer from '../../Footer/Footer';
 import Navibar from '../Navibar/Navibar';
 import "./styles.css"
+import { Container } from 'react-bootstrap';
+import backgroundImage from "../../../icon/backgroundImg.jpeg";
+
 
 const About = () => {
     return (
         <div className="about">
-            <Navibar />
+            <Container fluid style={{
+            backgroundImage: `url(${backgroundImage}`,
+            height: "100vh",
+            width: "100%",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover"
+         }}>
+         <Navibar />
+         </Container>
             <div className="site_title">
                 <h1 className="title_text_1">Качественный стайлинг</h1>
                 <div className="title_text_2">для Вашего автомобиля в Москве</div>
--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,21 +1,10 @@
-  import react from 'react';
-import { Container } from 'react-bootstrap';
-import backgroundImage from "../../icon/backgroundImg.jpeg";
 import Navibar from "./Navibar/Navibar";
 import Footer from "../Footer/Footer"
 
 const HomePage = () => {
    return (
       <>
-         <Container fluid style={{
-            backgroundImage: `url(${backgroundImage}`,
-            height: "100vh",
-            width: "100%",
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "cover"
-         }}>
          <Navibar />
-         </Container>
          <Footer />
       </>
    )
