68af9e6d6008998ef307bf191518da55d81c2f41
Djabrail Magamadov <magamadov.ru095@gmail.com>
2022-07-26 15:52:59 +0300

    Исправил map

--- src/components/pages/Home/HomePage.jsx
+++ src/components/pages/Home/HomePage.jsx
@@ -1,12 +1,13 @@
 import { Container } from 'react-bootstrap';
-import backgroundImage from "../../../icon/backgroundImg.jpeg";
-import Navibar from "../Navibar/Navibar";
-import Footer from "../../Layout/Footer/Footer"
 import LearnMore from "../Learn-More/LearnMore"
 
 const HomePage = () => {
    return (
       <>
+         <Container fluid className="main_content" style={{ color: "white", textAlign: "center" }}>
+            <h1>АВТОТЮНИНГ</h1>
+      
+         </Container>
          <LearnMore />
       </>
    )
