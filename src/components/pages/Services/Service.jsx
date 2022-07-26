3cde9a9ee4c6b0f8e0b05ba8c3e82c87c99287e1
Zelim <anzorov.zelim@gmail.com>
2022-07-26 16:40:02 +0300

    изменил стили на услуги

--- src/components/pages/Services/Service.jsx
+++ src/components/pages/Services/Service.jsx
@@ -7,14 +7,15 @@ function Service({ item }) {
     <div
       style={{
         backgroundImage: `url(${`http://localhost:4000${item.img}`})`,
-      
       }}
       className={styles.services_content}
     >
       <div className={styles.title_and_btn}>
         <div className={styles.element_title}>{item.title}</div>
         <div className={styles.element_button}>
-            <button className={styles.btn}><Link to="/servic"> Подробнее</Link></button>
+          <button className={styles.btn}>
+            <Link to="/servic">подробнее</Link>
+          </button>
         </div>
       </div>
     </div>
