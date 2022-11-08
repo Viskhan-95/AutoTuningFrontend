1f1762aaecb02fe26233b32ec59906e9879b970c
Djabrail Magamadov <magamadov.ru095@gmail.com>
2022-11-08 14:21:38 +0300

    render

--- src/server.js
+++ src/server.js
@@ -5,5 +5,5 @@ export let serverUrl;
 if(NODE_ENV === 'development') {
   serverUrl = 'http://localhost:4000' 
 } else {
-  serverUrl = 'https://app.onrender.com'; 
+  serverUrl = 'https://auto-backend.onrender.com'; 
 }
